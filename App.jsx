7e6372c7d48db78bbc1a62360355ea28986b87c1
saadjutt10 <saadgondal203@gmail.com>
2024-05-25 22:54:36 +0500

    Recovery

--- App.jsx
+++ App.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import ManageClasses from './src/Screens/ManageClasses';
 import ClassDetail from './src/Screens/ClassDetail';
 import AddClassScreen from './src/Screens/AddClassScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, CommonActions } from '@react-navigation/native';
-import { BottomNavigation, Icon, IconButton } from 'react-native-paper';
+import { BottomNavigation, IconButton } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
 import tw from 'twrnc';
-const Tab = createBottomTabNavigator();
 
-const navigationScreens = ["Dashboard", "Teachers", "Students", "Classes"]
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
@@ -17,50 +18,85 @@ const App = () => {
           headerShown: false,
           tabBarShowLabel: false, // Hide labels
         }}
-        
+        tabBar={({ navigation, state, descriptors, insets }) => (
+          <BottomNavigation.Bar
+            navigationState={state}
+            safeAreaInsets={insets}
+            onTabPress={({ route, preventDefault }) => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
+
+              if (event.defaultPrevented) {
+                preventDefault();
+              } else {
+                navigation.dispatch({
+                  ...CommonActions.navigate(route.name, route.params),
+                  target: state.key,
+                });
+              }
+            }}
+            renderIcon={({ route, focused, color }) => {
+              const { options } = descriptors[route.key];
+              if (options.tabBarIcon) {
+                return (
+                  <View >
+                    {options.tabBarIcon({ focused, color, size: 40 })}
+                  </View>
+                );
+              }
+
+              return null;
+            }}
+          />
+        )}
       >
         <Tab.Screen
           name="AddClassScreen"
           component={AddClassScreen}
           options={{
-            tabBarIcon: () => {
-              return <IconButton
+            tabBarIcon: ({ focused }) => (
+              <IconButton
                 icon="view-dashboard"
                 size={20}
-                iconColor='red' 
+                iconColor={focused ? 'white' : 'red'} // Change icon color based on focus
               />
-            },
+            ),
           }}
         />
         <Tab.Screen
           name="ManageClasses"
           component={ManageClasses}
           options={{
-            tabBarIcon: () => {
-              return <IconButton
+            tabBarIcon: ({ focused }) => (
+              <IconButton
                 icon="account-supervisor-circle"
                 size={20}
-                iconColor='red' 
+                iconColor={focused ? 'white' : 'red'} // Change icon color based on focus
               />
-            },
+            ),
           }}
         />
         <Tab.Screen
           name="ClassDetail"
           component={ClassDetail}
           options={{
-            tabBarIcon: () => {
-              return <IconButton
+            tabBarIcon: ({ focused }) => (
+              <IconButton
                 icon="account-tie"
                 size={20}
-                iconColor='red' 
+                iconColor={focused ? 'white' : 'red'} // Change icon color based on focus
               />
-            },
+            ),
           }}
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
 };
+
+
 
 export default App;
