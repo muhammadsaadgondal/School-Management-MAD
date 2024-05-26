0b946b5ad1f7b00e1c35f7a09cfb19e23d588929
saadjutt10 <saadgondal203@gmail.com>
2024-05-26 13:59:04 +0500

    Registration Class complete

--- App.jsx
+++ App.jsx
@@ -7,6 +7,7 @@ import { NavigationContainer, CommonActions } from '@react-navigation/native';
 import { BottomNavigation, IconButton } from 'react-native-paper';
 import { StyleSheet, View } from 'react-native';
 import tw from 'twrnc';
+import RegisterScreen from './src/Screens/RegisterScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,54 +15,24 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
+
         screenOptions={{
           headerShown: false,
           tabBarShowLabel: false, // Hide labels
         }}
-        tabBar={({ navigation, state, descriptors, insets }) => (
-          <BottomNavigation.Bar
-            navigationState={state}
-            safeAreaInsets={insets}
-            onTabPress={({ route, preventDefault }) => {
-              const event = navigation.emit({
-                type: 'tabPress',
-                target: route.key,
-                canPreventDefault: true,
-              });
 
-              if (event.defaultPrevented) {
-                preventDefault();
-              } else {
-                navigation.dispatch({
-                  ...CommonActions.navigate(route.name, route.params),
-                  target: state.key,
-                });
-              }
-            }}
-            renderIcon={({ route, focused, color }) => {
-              const { options } = descriptors[route.key];
-              if (options.tabBarIcon) {
-                return (
-                  <View >
-                    {options.tabBarIcon({ focused, color, size: 40 })}
-                  </View>
-                );
-              }
-
-              return null;
-            }}
-          />
-        )}
       >
         <Tab.Screen
-          name="AddClassScreen"
-          component={AddClassScreen}
+          name="RegisterScreen"
+          component={RegisterScreen}
           options={{
             tabBarIcon: ({ focused }) => (
               <IconButton
+                style={tw`bg-${focused ? 'red-300' : 'white'}`}
                 icon="view-dashboard"
                 size={20}
-                iconColor={focused ? 'white' : 'red'} // Change icon color based on focus
+                iconColor='red' // Change icon color based on focus
+
               />
             ),
           }}
@@ -72,9 +43,10 @@ const App = () => {
           options={{
             tabBarIcon: ({ focused }) => (
               <IconButton
+                style={tw`bg-${focused ? 'red-300' : 'white'}`}
                 icon="account-supervisor-circle"
                 size={20}
-                iconColor={focused ? 'white' : 'red'} // Change icon color based on focus
+                iconColor='red'// Change icon color based on focus
               />
             ),
           }}
@@ -85,9 +57,10 @@ const App = () => {
           options={{
             tabBarIcon: ({ focused }) => (
               <IconButton
+                style={tw`bg-${focused ? 'red-300' : 'white'}`}
                 icon="account-tie"
                 size={20}
-                iconColor={focused ? 'white' : 'red'} // Change icon color based on focus
+                iconColor='red'
               />
             ),
           }}
