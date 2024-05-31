3d30a5d93576e6e9eddd4b506c3a6cc6f16f6111
Rohail-Suii <125988921+Rohail-Suii@users.noreply.github.com>
2024-05-31 16:00:27 +0500

    use side displaying

--- src/Screens/userstudent/SyllabusScreen.jsx
+++ src/Screens/userstudent/SyllabusScreen.jsx
@@ -1,17 +1,26 @@
-// screens/SyllabusScreen.js
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import React, { useEffect, useState, useContext } from 'react';
+import { View, Text, FlatList, TouchableOpacity, Linking } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+import { AuthContext } from '../../auth/AuthContext';
+import { WebView } from 'react-native-webview';
 
 const SyllabusScreen = () => {
-  const [syllabus, setSyllabus] = useState([]);
+  const [syllabusUrl, setSyllabusUrl] = useState('');
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchSyllabus = async () => {
       try {
-        const syllabusSnapshot = await firestore().collection('Syllabus').get();
-        const syllabusData = syllabusSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        setSyllabus(syllabusData);
+        // Extract class ID from user's registration number
+        const classId = user.data.regNo.substring(0,1); // Assuming registration number is like "004" and class ID is "0"
+        console.log('Class ID:', classId);
+
+        // Fetch class data from Firestore
+        const classData = await firestore().collection('Class').doc(classId).get();
+        console.log('Class Data:', classData.data());
+
+        const classSyllabusUrl = classData.data().syllabusUrl;
+        setSyllabusUrl(classSyllabusUrl);
       } catch (error) {
         console.error('Error fetching syllabus:', error.message);
       }
@@ -20,20 +29,25 @@ const SyllabusScreen = () => {
     fetchSyllabus();
   }, []);
 
+
+
+  const openSyllabusUrl = () => {
+    if (syllabusUrl) {
+      Linking.openURL(syllabusUrl);
+    }
+  };
+
   return (
     <View>
       <Text>Syllabus Screen</Text>
-      <FlatList
-        data={syllabus}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.subject}: {item.topics}</Text>
-          </View>
-        )}
-        keyExtractor={(item) => item.id}
-      />
+      {syllabusUrl ? (
+        <WebView source={{ uri: syllabusUrl }} />
+      ) : (
+        <Text>No syllabus available</Text>
+      )}
     </View>
   );
+  
 };
 
 export default SyllabusScreen;
